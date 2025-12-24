0672721d5b1c0248cfafb80325c593528acafdc6
Sujon <abdur.sujon.dev@gmail.com>
2025-12-24 00:19:59 +0000

    Refactor UserData class for improved readability

--- Models/UserData.php
+++ Models/UserData.php
@@ -1,53 +1,39 @@
 <?php
 
 /**
- * Class UserData is designed to initialise get functions
- * to retrieve user data.
- *
- * It works as a data model for UserDataSet class.
+ * Data model representing a single user record.
+ * Encapsulates user attributes loaded from the database and
+ * provides read-only access to authentication and identity data.
  */
 class UserData {
- /**
-  * @var int $_id User's unique identifier.
-  * @var string $_username User's username.
-  * @var string $_email User's email address.
-  * @var string $_password_hash User's hashed password.
-  * @var string $_role User's role (Owner/User).
-  */
- protected $_id, $_username, $_email,
-           $_password_hash, $_role;
+    protected $_id, $_username, $_email,
+              $_password_hash, $_role;
 
- // Constructor of class UserData
- public function __construct($dbRow) {
-  $this->_id = $dbRow['id'];
-  $this->_username = $dbRow['username'];
-  $this->_email = $dbRow['email'];
-  $this->_password_hash = $dbRow['password_hash'];
-  $this->_role = $dbRow['role'];
- }
+    public function __construct($dbRow) {
+        $this->_id = $dbRow['id'];
+        $this->_username = $dbRow['username'];
+        $this->_email = $dbRow['email'];
+        $this->_password_hash = $dbRow['password_hash'];
+        $this->_role = $dbRow['role'];
+    }
 
- // Get user id
- public function getUserId() {
-  return $this->_id;
- }
+    public function getUserId() {
+        return $this->_id;
+    }
 
- // Get username
- public function getUsername() {
-  return $this->_username;
- }
+    public function getUsername() {
+        return $this->_username;
+    }
 
- // Get user email address
- public function getEmail() {
-  return $this->_email;
- }
+    public function getEmail() {
+        return $this->_email;
+    }
 
- // Get password hashed in the database
- public function getPasswordHash() {
-  return $this->_password_hash;
- }
+    public function getPasswordHash() {
+        return $this->_password_hash;
+    }
 
- // Get user role (Owner/User)
- public function getRole() {
-  return $this->_role;
- }
+    public function getRole() {
+        return $this->_role;
+    }
 }
