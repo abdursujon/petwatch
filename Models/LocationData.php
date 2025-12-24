0120e1a0fddae4b1c77461b2ea186d4143623ee0
Sujon <abdur.sujon.dev@gmail.com>
2025-12-24 00:17:13 +0000

    Update LocationData.php

--- Models/LocationData.php
+++ Models/LocationData.php
@@ -1,54 +1,38 @@
 <?php
 
 /**
- * Class LocationData
- *
- * Works as a data model for LocationDataSet class
+ * Data model representing a single location record.
+ * Encapsulates location attributes associated with a pet sighting
+ * and provides read-only access to persisted location data.
  */
 class LocationData {
- /**
-  * @var int $_id unique location id generated in db
-  * @var string $_pet_id foreign key to get pet_id and sightings locations
-  * @var string $_latitude sightings latitude
-  * @var string $_longitude sightings longitude
-  * @var string $_timestamp sighted time
-  */
- protected $_id, $_pet_id, $_latitude, $_longitude, $_timestamp;
+    protected $_id, $_pet_id, $_latitude, $_longitude, $_timestamp;
 
- /**
-  * LocationData constructor.
-  * Initializes the object with data from a database row.
-  */
- public function __construct($dbRow) {
-  $this->_id = $dbRow['id'];
-  $this->_pet_id = $dbRow['pet_id'];
-  $this->_latitude = $dbRow['latitude'];
-  $this->_longitude = $dbRow['longitude'];
-  $this->_timestamp = $dbRow['timestamp'];
- }
+    public function __construct($dbRow) {
+        $this->_id = $dbRow['id'];
+        $this->_pet_id = $dbRow['pet_id'];
+        $this->_latitude = $dbRow['latitude'];
+        $this->_longitude = $dbRow['longitude'];
+        $this->_timestamp = $dbRow['timestamp'];
+    }
 
- // Get the location id
- public function getLocationId() {
-  return $this->_id;
- }
+    public function getLocationId() {
+        return $this->_id;
+    }
 
- // Get the pet ID
- public function getPetId() {
-  return $this->_pet_id;
- }
+    public function getPetId() {
+        return $this->_pet_id;
+    }
 
- // Get the location latitude
- public function getLatitude() {
-  return $this->_latitude;
- }
+    public function getLatitude() {
+        return $this->_latitude;
+    }
 
- // Get the location longitude
- public function getLongitude() {
-  return $this->_longitude;
- }
+    public function getLongitude() {
+        return $this->_longitude;
+    }
 
- // Get the sighted time when location was recorded
- public function getTimestamp() {
-  return $this->_timestamp;
- }
+    public function getTimestamp() {
+        return $this->_timestamp;
+    }
 }
