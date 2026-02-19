a0f529cb8697875ee15e3fe30e391e1e9ff3789d
Sujon <abdur.sujon.dev@gmail.com>
2026-02-19 20:07:16 +0000

    ajax and json explored

---
+++ ajax/hint.php
@@ -0,0 +1,20 @@
+<?php
+$a = ["Linda", "Kaffi", "Malta", "Football", "Sujon", "Ryan", "Rakib", "Rahul", "Rikab"];
+
+$q = $_REQUEST["q"];
+$hint = "";
+
+if ($q !== "") {
+    $q = strtolower($q);
+    $len=strlen($q);
+    foreach($a as $name){
+        if(stristr($q, substr($name, 0, $len))){
+            if ($hint === ""){
+                $hint = $name;
+            } else{
+                $hint .= ", $name";
+            }
+        }
+    }
+}
+echo $hint === "" ? "no suggestion" : $hint;
