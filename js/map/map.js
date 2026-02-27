9caa3afc769f341c8d1f9272d11912abea8e652d
Sujon <abdur.sujon.dev@gmail.com>
2026-02-27 01:53:42 +0000

    Some update on map

--- js/map/map.js
+++ js/map/map.js
@@ -24,6 +24,7 @@ xhr.send();
 
 let popupOption = { "closeButton": false}
 
+// Ajax
 xhr.onreadystatechange = function(){
     if(xhr.readyState === 4 && xhr.status === 200){
         var data = JSON.parse(xhr.responseText);
